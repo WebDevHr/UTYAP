ff84a28db23f0a55e4bccb2bf1f28866f93f118a
Hossein Razeghian <razeqian@gmail.com>
2024-03-31 00:39:37 +0300

    lazy components

--- pages/index.vue
+++ pages/index.vue
@@ -4,20 +4,18 @@
       <NuxtParticles id="tsparticles" :options="options" />
       <div
         class="font-sans bg-transparent mx-auto max-w-7xl items-center justify-between py-4 px-2 sm:px-6 lg:px-8 mb-10">
-        <AudioPlayer :audioSrc="interstellar" :autoplay="loaded" class="fixed bottom-5 right-0 z-[1001]" />
+        <LazyHomepageAudioPlayer :audioSrc="interstellar" :autoplay="loaded" class="fixed bottom-5 right-0 z-[1001]" />
         <HeroSection />
         <SliderComponent />
       </div>
     </div>
 
-    <CardsComponent />
-    <LazyLoad :threshold="0.2">
-      <AboutUsCoomponent />
-    </LazyLoad>
+    <LazyHomepageCardsComponent />
+    <LazyHomepageAboutUsComponent />
 
-    <VisionMissionComponent />
-    <PhotoGalleryComponent />
-    <ContactUsComponent />
+    <LazyHomepageVisionMissionComponent />
+    <LazyHomepagePhotoGalleryComponent />
+    <LazyHomepageContactUsComponent />
 
   </div>
 </template>
@@ -26,15 +24,8 @@
 
 import { ref, onMounted } from 'vue'
 import options from '~/assets/particles/particles.json'
-import options2 from '~/assets/particles/particles-background.json'
-import AudioPlayer from '~/components/homepage/AudioPlayer.vue';
-import CardsComponent from '~/components/homepage/CardsComponent.vue';
 import HeroSection from '~/components/homepage/HeroSection.vue';
 import SliderComponent from '~/components/homepage/SliderComponent.vue';
-import AboutUsCoomponent from '~/components/homepage/AboutUsCoomponent.vue';
-import VisionMissionComponent from '~/components/homepage/VisionMissionComponent.vue';
-import PhotoGalleryComponent from '~/components/homepage/PhotoGalleryComponent.vue';
-import ContactUsComponent from '~/components/homepage/ContactUsComponent.vue';
 
 import { tsParticles } from 'tsparticles-engine'
 import { loadFull } from 'tsparticles' // or whichever bundle you wish to use
